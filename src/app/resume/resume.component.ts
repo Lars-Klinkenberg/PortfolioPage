0d59c91e149b4b611f1118ef092115fe932cdbea
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-03-23 16:04:03 +0100

    iterate over skills

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { SkillIndexComponent } from './components/skill-index/skill-index.component';
+import myResume from './resume.data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [SkillIndexComponent],
+  imports: [SkillIndexComponent, CommonModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
 export class ResumeComponent {
+  resumeData = myResume;
 
-  testSkill = "testSkillName"
 }
