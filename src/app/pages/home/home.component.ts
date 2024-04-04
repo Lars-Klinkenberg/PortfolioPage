801b53b8d4891cd7f50e904b94e81cbfb2184a37
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2024-04-04 22:57:18 +0200

    added contact

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PageIndicatorComponent } from '../../shared/page-indicator/page-indicator.component';
 import { Skill } from '../../shared/skill-set/Skill';
 import { SkillSetComponent } from '../../shared/skill-set/skill-set.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PageIndicatorComponent, SkillSetComponent],
+  imports: [PageIndicatorComponent, SkillSetComponent, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   skills: Skill[] = [
     {
       name: 'Vue',
@@ -49,4 +50,12 @@ export class HomeComponent {
       iconPath: 'github-mark-white.png',
     },
   ];
+
+  message = new FormControl('');
+  email = new FormControl('');
+
+  ngOnInit() {}
+
+  updateForm() {
+  }
 }
